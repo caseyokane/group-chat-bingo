c0d5678506cb7917635de39c31a8f9907899edc2
caseyokane <casey.okane@uky.edu>
2018-05-28 10:21:38 -0400

    Include Angular Material to display Bingo header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BingoBoardComponent } from './bingo-board/bingo-board.component';
+
+import {MatButtonModule, MatCardModule, MatGridListModule, MatToolbarModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BingoBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
