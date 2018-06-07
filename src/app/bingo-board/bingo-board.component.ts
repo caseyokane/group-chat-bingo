f9ac6d4de81c9737a183b4cc5ba6f0f57914455b
Casey Dale Spires <casey.spires@eku.edu>
2018-06-07 07:58:04 -0400

    Added win scenarios for Row and Column

--- src/app/bingo-board/bingo-board.component.ts
+++ src/app/bingo-board/bingo-board.component.ts
@@ -27,16 +27,35 @@ export class BingoBoardComponent implements OnInit {
     const count = this.subjects.length;
     let place = 0;
     // Set Rows
-    for (let row = 1; row < this.rows; row++) {
+    for (let row = 1; row <= this.rows; row++) {
       // Set Columns
-      for (let column = 1; column < this.columns; column++) {
+      for (let column = 1; column <= this.columns; column++) {
         this.subjects[place].row = row;
         this.subjects[place].col = column;
         place++;
       }
     }
   }
-
+  rowWin(): void {
+    for (let row = 0; row <= this.rows; row++) {
+      const result = this.subjects.filter(subject => subject.row === row && subject.selected === true).length;
+      if (result === 5) {
+        window.alert('win');
+      }
+    }
+  }
+  columnWin(): void {
+    for (let col = 0; col <= this.columns; col++) {
+      const result = this.subjects.filter(subject => subject.col === col && subject.selected === true).length;
+      if (result === 5) {
+        window.alert('win');
+      }
+    }
+  }
+  checkWin() {
+    this.rowWin();
+    this.columnWin();
+  }
   getSubjects(): void {
     this.bingoBoardService.getSubjects()
         .subscribe(subject => this.subjects = subject);
@@ -44,6 +63,7 @@ export class BingoBoardComponent implements OnInit {
 
   onSelect(subject: Subject): void {
     this.subjects[subject.id - 1].selected = !this.subjects[subject.id - 1].selected;
+    this.checkWin();
   }
 
 }
--- src/app/subject.ts
+++ src/app/subject.ts
@@ -2,6 +2,6 @@ export class Subject {
   id: number;
   name: string;
   selected: boolean;
-  row: number;
-  col: number;
+  row?: number;
+  col?: number;
 }
